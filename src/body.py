4f56a2ec6da994cb7914b537b5e0b08fc6431bdf
HJack69 <69hjack69@gmail.com>
2024-12-14 16:51:17 +0100

    Update welcome event

--- src/body.py
+++ src/body.py
@@ -1,9 +1,11 @@
+from src.db_classes import WelcomeMessages
 from src.tasks import club_event_reminder, game_reset_reminder, my_midnight_reminder, game_midnight_reminder
 from src.variables import local_deploy, server_id, channel_ids, channel_ids_test, webhook_id
 from src.views import WelcomeView
 
 import re
 
+from discord.errors import NotFound
 from discord.ext import commands
 from discord.flags import Intents
 
@@ -42,14 +44,17 @@ class BOT(commands.Bot):
         my_midnight_reminder.start(server)
         game_reset_reminder.start(server)
 
-        channel = server.get_channel(channel_ids["welcome"])
-
-        #TODO: save the ids in db to not loop over channel
-        for message in [message async for message in channel.history(limit=None) if message.author.id == webhook_id and message.components][::-1]:
-            user_id = re.sub(pattern=r'''\D+''', repl="", string=message.content)
-            user = server.get_member(int(user_id))
-            
-            self.add_view(view=WelcomeView(user=user, stickers=server.stickers), message_id=message.id)
+        for message_id in WelcomeMessages().get_message_ids()[::-1]:
+            try:
+                channel = server.get_channel(channel_ids["welcome"])
+                message = await channel.fetch_message(message_id)
+                
+                user_id = re.sub(pattern=r'''\D+''', repl="", string=message.content)
+                user = server.get_member(int(user_id))
+                
+                self.add_view(view=WelcomeView(user=user, stickers=server.stickers), message_id=message.id)
+            except NotFound:
+                pass
         
         ### TESTS HERE ###
         if local_deploy:
