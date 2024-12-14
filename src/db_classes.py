df6e72b3d62a7a6300a47d2867d2c1569e1e918d
HJack69 <69hjack69@gmail.com>
2024-12-14 16:51:17 +0100

    Update welcome event

--- src/db_classes.py
+++ src/db_classes.py
@@ -2,37 +2,90 @@ from src.variables import db_connection, db_cursor
 
 from datetime import datetime, timedelta
 
-__all__ = ["ExtraVariables"]
+__all__ = ["ExtraVariable", "WelcomeMessages"]
 
 
-class ExtraVariables:
-    
+class Database():
     @classmethod
     def set_(cls, db_connection, db_cursor):
         cls.con = db_connection
         cls.cur = db_cursor
 
+    def select_from(self, table, id=None):
+        add = (f" WHERE NAME = '{id}'" if table == "sqlite_sequence" else f" WHERE ID = {id}") if id else ""
+        self.cur.execute(f"SELECT * FROM {table}{add};")
+        
+        all, item = {}, ()
+        for item in self.cur:
+            all[item[1:]] = item[0]
+        
+        if id:
+            return item
+        else:
+            return all
+    
+    def get_columns(self, table, drop_id=True):
+        self.cur.execute(f"PRAGMA table_info({table});")
+        
+        if drop_id:
+            return [item[1] for item in self.cur if item[1] != "id"]
+        else:
+            return [item[1] for item in self.cur]
+
+
+class ExtraVariable(Database):
     def __init__(self, id):
-        self.cur.execute(f"SELECT * FROM extra_variables WHERE ID = {id};")
-        self.id, self.type, self.name, var_int = [item for item in self.cur][0]
+        self.table = "extra_variables"
+        
+        self.id, self.type, self.name, var_int = self.select_from(self.table, id)
         self.value = self.get_value(var_int)
 
     def get_value(self, variable_int):
 
         if self.type == "bool":
             return bool(variable_int)
+        
+        elif self.type == "date":
+            return convert_to_date(variable_int)
 
     def change_value(self, new_value=None):
         
         # reverse value
         if self.type == "bool":
-            self.cur.execute(f"UPDATE extra_variables SET var_int = {int(not self.value)} WHERE ID = {self.id};")
+            self.cur.execute(f"UPDATE {self.table} SET var_int = {int(not self.value)} WHERE ID = {self.id};")
             self.value = not self.value
         
         self.con.commit()
 
 
-ExtraVariables.set_(db_connection, db_cursor)
+class WelcomeMessages(Database):
+    def __init__(self):
+        self.table = "welcome_messages"
+        self.all = self.select_from(self.table)
+
+    # add message_id to db
+    def add_message_id(self, message_id):
+        new_record = (message_id,)
+        
+        try:
+            self.all[new_record]
+            print("MESSAGE_ID ALREADY IN DATABASE!")
+        except KeyError:            
+            _, id = self.select_from(table="sqlite_sequence", id=self.table)
+
+            columns = ", ".join(self.get_columns(self.table))
+            values = ", ".join(str(x) for x in new_record)
+
+            self.cur.execute(f"INSERT INTO {self.table} ({columns}) VALUES ({values});")
+            self.all[new_record] = int(id) + 1
+            
+            self.con.commit()
+    
+    def get_all(self):
+        return [record[0] for record in self.all.keys()]
+
+
+Database.set_(db_connection, db_cursor)
 
 
 def convert_to_date(date_in_int: int) -> datetime:
