98e391fb2e4cc86d67a65570628957e99616be99
HJack69 <69hjack69@gmail.com>
2024-12-17 12:03:54 +0100

    Add custom server deploy

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.commands import *
 from src.events import *
-from src.variables import bot_token, path
+from src.variables import bot_token, absolute_path
 
 __title__ = 'src'
-__path__ = path
+__path__ = absolute_path
