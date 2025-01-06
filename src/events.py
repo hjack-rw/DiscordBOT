f0a4ef8cca712649f5d2b7c084749df00c68c57a
HJack69 <69hjack69@gmail.com>
2025-01-06 14:32:38 +0100

    Update tasks system and add tasks for Filch

--- src/events.py
+++ src/events.py
@@ -18,15 +18,16 @@ if test_bot["test_events"]:
 # Welcoming event
 @bot.event
 async def on_member_join(new_user):
-    print("Recognised that a member called " + new_user.name + "," + f"{new_user.id}" + " joined")
-
     server = bot.get_guild(server_id)
     channel = server.get_channel(channel_ids["welcome"])
     
     image = draw_infocard(new_user=new_user, all_members=len([member for member in server.members if not member.bot]))
 
     embed = Embed(title=f"Welcome, {new_user.name}, to GatesOfPurgatory! <:hugs:1256225688403447888>",  description="Go to <id:guide> and follow the instructions :)", color=system_embed_color)
-    embed.set_image(url="attachment://card.png")    
+    embed.set_image(url="attachment://card.png")
+    embed.set_footer(text=f'''"You're a Wizard, {new_user.name}."''')
     
     message = await send_webhook(target_channel=channel, user_name="Prof. Hagrid", content=f"Mention: <@{new_user.id}>", embed=embed, file=image, view=WelcomeView(user=new_user, stickers=server.stickers))
-    WelcomeMessages().add_message_id(message.id)
+    
+    if not test_bot["test_events"]:
+        WelcomeMessages().add_message_id(message.id)
