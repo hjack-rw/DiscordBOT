f58fb066981cc9571ec34b5f8323c2ebc8dc1add
HJack69 <69hjack69@gmail.com>
2025-04-19 14:54:22 +0200

    Add "send_notification" command

--- src/events.py
+++ src/events.py
@@ -1,10 +1,9 @@
 from src.body import bot
 from src.db_classes import WelcomeMessages
-from src.functions import send_webhook, draw_infocard
-from src.variables import test_bot, server_id, channel_ids, channel_ids_test, system_embed_color
+from src.functions import draw_infocard
+from src.tasks import print_notification
+from src.variables import test_bot, server_id, channel_ids_test
 from src.views import WelcomeView
-
-from discord.embeds import Embed
 
 
 # SETTINGS
@@ -20,16 +19,12 @@ if test_bot["test_events"]:
 async def on_member_join(new_user):
     if not new_user.bot:
         server = bot.get_guild(server_id)
-        channel = server.get_channel(channel_ids["welcome"])
-        
-        image = draw_infocard(new_user=new_user, all_members=len([member for member in server.members if not member.bot]))
 
-        embed = Embed(title=f"Welcome, {new_user.name}, to GatesOfPurgatory! <:hugs:1256225688403447888>",  description="Go to <id:guide> and follow the instructions :)", color=system_embed_color)
-        embed.set_image(url="attachment://card.png")
-        embed.set_footer(text=f'''"You are a Wizard, {new_user.name}."''')
-        
-        message = await send_webhook(target_channel=channel, user_name="Prof. Hagrid", content=f"Mention: <@{new_user.id}>", embed=embed, file=image, view=WelcomeView(user=new_user, stickers=server.stickers))
-        
+        image = draw_infocard(new_user=new_user, all_members=len([member for member in server.members if not member.bot]))
+        view = WelcomeView(user=new_user, stickers=server.stickers)
+
+        message = print_notification(server, date=None, event_name="Welcome", variables=[new_user, image, view])
+
         if not test_bot["test_events"]:
             WelcomeMessages().add(message.id)
     else:
