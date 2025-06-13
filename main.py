4c5db7f1f05e7ee12ae678435e05f62355626e31
HJack69 <69hjack69@gmail.com>
2025-06-13 17:52:08 +0200

    Cleanup code

--- main.py
+++ main.py
@@ -4,30 +4,26 @@ if __name__ == '__main__':
     bot.run(bot_token)
 
 
-#TODO! remove past bot notifications on midnight (if exist), but not SNAPE
-# - also fix DELETE SNAPE!
-
 #TODO! rework the sorthing-hat channel:
 # ~house picking system and change nickname~
 # ~add the member list permanent~
 # - subscription system (see bellow:)
 
 #TODO! subscription system:
-# pick a subscription and add the role to members before the event
-# clear all subscriptions on another button
-# IMPORTANT: check if clearing the role keeps the notification!
+# - pick a subscription and add the role to members before the event
+# - clear all subscriptions on another button
+# - IMPORTANT: check if clearing the role keeps the notification!
 
 #TODO! sprout:
 # trigger on herbology related stuff:
-# weekly plants,
-# own timers for plants with notification for the server:
-
-# - for own timers use create_a_task that is run on restart of the bot:
+# - weekly plants,
+# - own timers for plants with notification for the server:
+# -- for own timers use create_a_task that is run on restart of the bot:
 # create_a_task(timer={"hours":0, "minutes":0, "seconds":0}).start(event_info={"id": 1})
 
-# - the times are from the database. while creating save to database. after executing delete
-# - limit for user that is set in code (2 for testing)
-# - seperate into aquatic and non aquatic
+# -- the times are from the database. while creating save to database. after executing delete
+# -- limit for user that is set in code (2 for testing)
+# -- seperate into aquatic and non aquatic
 
 
 ## CRAZY IDEAS ##
@@ -42,7 +38,7 @@ if __name__ == '__main__':
 # - when they trigger normally just remove them
 
 #TODO! portkey:
-# automatic add to a paste service
+# - automatic add to a paste service
 
 #TODO! db changes:
 # - insert without defaults if provided
